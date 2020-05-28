f77b4a9ee20e490d2b61934419002b403e838e73
Ryan Gabriel <48722498+ryangabriel142@users.noreply.github.com>
2020-05-28 15:23:33 -0300

    Create formcadastro.php

---
+++ formcadastro.php
@@ -0,0 +1,10 @@
+<form action="controllerCadastro.php" method="post">
+	<label>Nome: </label>
+	<input type="text" name="nome"><br>
+	<label>E-mail: </label>
+	<input type="email" name="email"><br>
+	<label>Telefone: </label>
+	<input type="text" name="telefone"><br>
+	
+	<input type="submit" value="GRAVAR">
+</form>
